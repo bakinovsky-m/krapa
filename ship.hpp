5c5f5e099e8a6fe0c36babff9deeaca8cbd5d188
ilpech <i.v.pichugin@yandex.ru>
2018-12-22 17:52:04 +0300

    solved merg

--- ship.hpp
+++ ship.hpp
@@ -3,33 +3,51 @@
 
 #include <string>
 #include "cargo_type.hpp"
+//#include "port.hpp"
+
 
 class Ship{
 public:
-  Ship(std::string name_, double wght, int time_in_query_, double rate_, CargoType cargo_) :
+  Ship(std::string name_, double wght, int time_in_query_, double rate_, CargoType cargo_, Port port) :
     name(name_),
     weight(wght),
     time_in_query(time_in_query_),
     rate(rate_),
-    cargo_type(cargo_)
+    cargo(cargo_),
+    port(port_)
   {}
 
   std::string name;
-  CargoType cargo_type;
+  CargoType cargo;
 
+//  Port port;
   double weight;
   int time_in_query;
   double rate;
+  int time_to_unload;
 
+  void countRate();
+  void countTimeToUnload();
+  void stepInQuery();
 
   void countRate();
 };
+
+void Ship::countTimeToUnload()
+{
+    time_to_unload = weight / port.unloadingSpeed;
+}
+
+void Ship::stepInQuery()
+{
+    time_in_query++;
+}
 
 
 void Ship::countRate()
 {
 
-    rate = weight * cargo_type.koefPorchi * cargo_type.importance * time_in_query;
+    rate = weight * cargo.koefPorchi * cargo.importance * time_in_query;
 
 }
 
