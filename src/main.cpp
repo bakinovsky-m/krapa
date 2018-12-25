c80e5308f40bc60120a1933a143c161ffaeb9bf9
bakinovsky-m <makbak@ya.ru>
2018-12-26 00:59:36 +0300

    more random

--- src/main.cpp
+++ src/main.cpp
@@ -26,24 +26,32 @@ int main()
   p.addShip(Ship("carrot for poor rabbits", 12, &ct[1]));
   p.addShip(Ship("fucking carrot", 17, &ct[1]));
 
-  default_random_engine eng(1);
+  default_random_engine eng_for_intensity(1);
+  default_random_engine eng_for_port_speed = default_random_engine(2);
+  default_random_engine eng_for_cargo_amount = default_random_engine(3);
   normal_distribution<double> intensity{};
   normal_distribution<double> port_speed(1, 1);
+  normal_distribution<double> amount{};
 
   int a = 0;
-  while(!p.time_to_stop){
-    p.unloadingSpeed = fabs(ceil(port_speed(eng)));
-    if (fabs(intensity(eng)) > 0.99) {
-      p.addShip(Ship("asd", 1, &ct[0]));
+  while(!p.time_to_stop && a < 100){
+    p.unloadingSpeed = fabs(ceil(port_speed(eng_for_port_speed)));
+    if (fabs(intensity(eng_for_intensity)) > 0.99) {
+      p.addShip(Ship("asd", round(fabs(amount(eng_for_cargo_amount)) * 10), &ct[0]));
     }
-    cout << endl << "AFTER " << a << " TICK" << endl;
-    cout << "++++++++++++++++++++++++++++++++++++++++" << endl;
-    cout << "port speed: " << p.unloadingSpeed << endl;
+//    cout << endl << "AFTER " << a << " TICK" << endl;
+//    cout << "++++++++++++++++++++++++++++++++++++++++" << endl;
+//    cout << "port speed: " << p.unloadingSpeed << endl;
     tickCargoTypes(ct);
     p.tick();
-    p.print();
+//    p.print();
     a++;
-    cin.get();
+//    cin.get();
+  }
+
+  cout << "on tick " << a << endl;
+  for(auto c : ct) {
+    cout << c.name << ": " << c.cur_amount << "/" << c.max_amount << endl;
   }
 
   return 0;
