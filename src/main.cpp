7c9377bfa9e6304d635bf4f5c961a2ccc1dac566
bakinovsky-m <makbak@ya.ru>
2018-12-26 12:15:05 +0300

    asd

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,8 @@ int main()
 {
   vector<CargoType> ct{};
   // TODO: random na max_amount
-  ct.push_back(CargoType("carrot", 1.23, 1000, 20, 2, 12));
-  ct.push_back(CargoType("nasvay", 1.01, 1000, 7, 1, 9));
+  ct.push_back(CargoType("carrot", 1.23, 1000, 20, 2, 1000));
+  ct.push_back(CargoType("nasvay", 1.01, 1000, 7, 1, 1000));
   Port p{};
   p.addShip(Ship("nasvay bringer", 2, &ct[0]));
 //  p.addShip(Ship("carrot for poor rabbits", 12, &ct[1]));
@@ -44,34 +44,40 @@ int main()
   int ships = 0;
 
   int a = 0;
-  while(!p.time_to_stop && a < 1000){
+  while(!p.time_to_stop && a < 10000){
     p.unloadingSpeed = fabs(ceil(port_speed(eng_for_port_speed)));
     if (fabs(intensity(eng_for_intensity)) > 0.5) {
       double q = cargo_type_index(eng_for_cargo_type);
-      int qwe = (int)round(q);
+      int qwe = static_cast<int>(round(q));
       mid_index += q;
       int asd = amount_int(eng_for_amount_int) * 100 + amount_(eng_for_amount);
       p.addShip(Ship("asd", asd, &ct[qwe]));
       full_amount = (full_amount + asd);
       ships++;
     }
-//    cout << endl << "AFTER " << a << " TICK" << endl;
-//    cout << "++++++++++++++++++++++++++++++++++++++++" << endl;
-//    cout << "port speed: " << p.unloadingSpeed << endl;
+    cout << endl << "AFTER " << a << " TICK" << endl;
+    cout << "++++++++++++++++++++++++++++++++++++++++" << endl;
+    cout << "port speed: " << p.unloadingSpeed << endl;
     tickCargoTypes(ct);
     p.tick();
 //    p.dumb_tick();
-//    p.print();
+    p.print();
+//    cout << "a: " << a << endl;
+//    cout << p.queue.size() << endl;
+//    cout << "on tick " << a << endl;
+    for(auto c : ct) {
+      cout << c.name << ": " << c.cur_amount << "/" << c.max_amount << ", importance: " << c.importance << endl;
+    }
     a++;
-//    cin.get();
+    cin.get();
   }
 
   cout << "on tick " << a << endl;
   for(auto c : ct) {
     cout << c.name << ": " << c.cur_amount << "/" << c.max_amount << endl;
   }
-  cout << "mid amount: " << full_amount/p.queue.size() << endl;
-  cout << "mid index: " << mid_index/p.queue.size() << endl;
+  cout << "mid amount: " << full_amount/ships << endl;
+  cout << "mid index: " << mid_index/ships << endl;
   cout << "ships: " << ships << endl;
 
   return 0;
